36ddc3113d5eb3160bc3a2790183d485eccfd56a
Trinadh <trinadhsuroju.s@gmail.com>
2025-08-06 18:52:31 +0530

    Added vercel insights 4.0

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import { HelmetProvider } from 'react-helmet-async'
 import App from './App.tsx'
 import './index.css'
 import { CartProvider } from './contexts/CartContext.tsx'
-
+import { SpeedInsights } from "@vercel/speed-insights/react"
 createRoot(document.getElementById("root")!).render(
   <HelmetProvider>
     <CartProvider>
